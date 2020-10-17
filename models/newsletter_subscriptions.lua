a62ad0d34c33d7c8915b801108194e1850f75370
Lleyton Gray <lleyton@innatical.com>
2020-10-16 21:46:05 -0700

    Update newsletter_subscriptions modek

--- models/newsletter_subscriptions.lua
+++ models/newsletter_subscriptions.lua
@@ -1,5 +1,7 @@
 local Model = require('lapis.db.model').Model
 
-local NewsletterSubscribers = Model:extend('newsletter_subscribers')
+local NewsletterSubscribers = Model:extend('newsletter_subscribers', {
+  primary_key = 'email'
+})
 
 return NewsletterSubscribers
