f9c83e24e6886665bd6f73c7bcf84ee10f54ec3f
Lleyton Gray <lleyton@innatical.com>
2020-11-17 23:54:28 -0800

    Fix community deletion

--- routes/communities/delete/community.lua
+++ routes/communities/delete/community.lua
@@ -3,8 +3,10 @@ local lapis = require 'lapis'
 local validate = require 'lapis.validate'
 local encoding = require 'lapis.util.encoding'
 local db = require 'lapis.db'
+local preload = require 'lapis.db.model'.preload
 local helpers = require 'lapis.application'
 local Members = require 'models.members'
+local broadcast = require 'util.broadcast'
 
 local inspect = require 'inspect'
 
@@ -15,13 +17,26 @@ return function(self)
   validate.assert_valid(self.params, {
     { 'id', exists = true, is_uuid = true, 'InvalidUUID' }
   })
-
+  -- TODO: NOT ATOMIC BUT OK
   local community = helpers.assert_error(Communities:find({ id = self.params.id }), 'CommunityNotFound')
   helpers.assert_error(community.owner_id == self.user_id, { 403, 'MissingPermissions' })
+  preload(community, 'members')
 
-  assert(community:delete())
+  for _, row in ipairs(community:get_members()) do
+    broadcast('user:' .. row.user_id, 'DELETED_MEMBER', {
+      id = row.id
+    })
+  end
+
+  assert(db.delete('communities', {
+    id = self.params.id
+  }))
+  -- community:delete() TODO: Causes error, let's file an issue.
   assert(db.delete('members', 'community_id = ?', self.params.id))
   assert(db.delete('channels', 'community_id = ?', self.params.id))
   -- TODO: Delete messages as well.
+
+  -- TODO: Inefficient, but /shrug
+
   return { layout = false }
 end
