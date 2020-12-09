6252e10568b512c32e75b12b61a6592de2be565c
Lleyton Gray <lleyton@innatical.com>
2020-12-09 09:17:13 -0800

    Make regexp case insensitive

--- util/validators/matches_regexp.lua
+++ util/validators/matches_regexp.lua
@@ -1,5 +1,6 @@
 local validate = require 'lapis.validate'
 
 function validate.validate_functions.matches_regexp(input, check)
-  return ngx.re.match(input, check), 'InvalidInput'
+  -- NOTE: Case insentitive.
+  return ngx.re.match(input, check, 'i'), 'InvalidInput'
 end
