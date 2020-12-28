fe76a2eba8d988a2bb92c2735097a5d138ef2dbc
Lleyton Gray <lleyton@innatical.com>
2020-12-28 13:50:11 -0800

    Security fixes and members search

--- routes/communities/init.lua
+++ routes/communities/init.lua
@@ -13,6 +13,7 @@ app:match('invites', '/:id/invites', guard(helpers.respond_to(require 'routes.co
 app:match('channels', '/:id/channels', guard(helpers.respond_to(require 'routes.communities.channels')))
 app:match('leave', '/:id/leave', guard(helpers.respond_to(require 'routes.communities.leave')))
 app:match('members', '/:id/members', guard(helpers.respond_to(require 'routes.communities.members')))
+app:match('members_search', '/:id/members/search', guard(helpers.respond_to(require 'routes.communities.members_search')))
 
 
 return app
