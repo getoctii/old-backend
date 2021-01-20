b386d10c1c0dec26a5d4c91522cf40b5571fbbc7
Adam <adam@atiktech.co>
2021-01-19 20:25:11 -0600

    ✨ added invite fetch endpoint

--- routes/invites/invite.lua
+++ routes/invites/invite.lua
@@ -1,9 +1,37 @@
 local validate = require 'lapis.validate'
 local helpers = require 'lapis.application'
 local Invites = require 'models.invites'
+local Communities = require 'models.communities'
 local db = require 'lapis.db'
 
 local Invite = {}
+
+function Invite:GET()
+  validate.assert_valid(self.params, {
+    { 'id', exists = true, is_uuid = true, 'InvalidCode' }
+  })
+
+  local invite = helpers.assert_error(Invites:find({ code = self.params.id }), 'InviteNotFound')
+  local community = invite:get_community()
+
+  return {
+    json = {
+      id = invite.id,
+      code = invite.code,
+      author_id = invite.author_id,
+      created_at = invite.created_at,
+      updated_at = invite.updated_at,
+      uses = invite.uses,
+      community = {
+        id = community.id,
+        name = community.name,
+        icon = community.icon,
+        large = community.large,
+        owner_id = community.owner_id
+      }
+    }
+  }
+end
 
 function Invite:DELETE()
   validate.assert_valid(self.params, {
