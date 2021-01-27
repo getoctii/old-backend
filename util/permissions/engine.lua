bb0788ab41b8959584ea664f17c6754aa8ecb79d
Lleyton Gray <lleyton@innatical.com>
2021-01-27 11:52:21 -0800

    Add support for base permissions to the engine

--- util/permissions/engine.lua
+++ util/permissions/engine.lua
@@ -12,6 +12,12 @@ end
 
 function engine.has_community_permissions(member_id, permissions)
   local member = MembersModel:find(member_id)
+  local community = MembersModel:get_community()
+
+  if member.user_id == community.owner_id then
+    return true
+  end
+
   local group_members = member:get_group_members()
   preload(group_members, 'group')
 
@@ -19,12 +25,11 @@ function engine.has_community_permissions(member_id, permissions)
     return group_member:get_group()
   end)
 
-  local total_permissions = engine.sum(array.map(groups, function(group)
+  local total_permissions = community.base_permissions + engine.sum(array.map(groups, function(group)
     return Set(group.permissions)
   end))
 
   return permissions < total_permissions
 end
--- ur cutting out badly lmao member list? yes thats for later
--- prob gonna display the top roles you can fit in ur screen then have a ... button that has a popup with the other roles u can manage simialr to discords member list
+
 return engine
