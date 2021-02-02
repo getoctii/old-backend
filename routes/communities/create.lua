efc13cd35b08d81178dc348732c408a9548132cd
Lleyton Gray <lleyton@innatical.com>
2021-02-02 13:03:33 -0800

    haha forgot to push

--- routes/communities/create.lua
+++ routes/communities/create.lua
@@ -6,6 +6,7 @@ local broadcast = require 'util.broadcast'
 local http = require 'resty.http'
 local helpers = require 'lapis.application'
 local uuid = require 'util.uuid'
+local Groups = require 'models.groups'
 
 local Create = {}
 
@@ -25,7 +26,8 @@ function Create:POST()
     name = self.params.name, -- TODO: Differenciate between query params and form
     icon = self.params.icon,
     large = true,
-    owner_id = self.user.id
+    owner_id = self.user.id,
+    base_permissions = { Groups.permissions.READ_MESSAGES, Groups.permissions.SEND_MESSAGES }
   }))
 
   local member = assert(Members:create({
