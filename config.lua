d93b502d333b4e748c7c005ba6bcf389f1c0e70b
Lleyton Gray <lleyton@innatical.com>
2021-04-18 02:04:36 +0000

    Update config.lua, .gitignore, .dockerignore files

---
+++ config.lua
@@ -0,0 +1,24 @@
+local config = require 'lapis.config'
+
+config('development', {
+  postgres = {
+    host = os.getenv('NOMAD_UPSTREAM_ADDR_postgres'),
+    user = tonumber(os.getenv('NOMAD_UPSTREAM_PORT_postgres')),
+    password = os.getenv('POSTGRES_PASSWORD'),
+    database = 'octii',
+    port = 5432
+  },
+  jwt = {
+    public = 'keys/public.pem',
+    private = 'keys/private.pem'
+  },
+  code_cache = 'off',
+  default_profile_pictures = {
+    'img1',
+    'img2'
+  },
+  ssl_certificate = '/etc/ssl/cert.pem',
+  port = tonumber(os.getenv('NOMAD_PORT_gateway')),
+  pushpin = 'http://' .. os.getenv('NOMAD_ADDR_pushpin_admin'),
+  resolver = '127.0.0.11 ipv6=off'
+})
