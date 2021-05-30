01ea309b28a60ed94f5e8e5a4a804ce87b097a82
Lleyton Gray <lleyton@innatical.com>
2021-05-29 18:38:15 -0700

    push updated config

--- config.lua
+++ config.lua
@@ -1,5 +1,12 @@
 local config = require 'lapis.config'
 
+local function read_file(filename)
+  local file = io.open(filename)
+  local data = file:read("*a")
+  file:close()
+  return data
+end
+
 config('development', {
   postgres = {
     host = 'postgres',
@@ -28,4 +35,35 @@ config('development', {
   port = 8086,
   pushpin = 'http://pushpin:5561',
   resolver = '127.0.0.11'
+})
+
+config('production', {
+  postgres = {
+    host = 'db.octii.chat',
+    user = 'OLD_MONOLITH',
+    password = read_file('/run/secrets/db_password'),
+    database = 'MONOLITH',
+  },
+  jwt = {
+    public = '/run/secrets/auth_public',
+    private = '/run/secrets/auth_private',
+    voice = '/run/secrets/voice_private'
+  },
+  code_cache = 'on',
+  default_profile_pictures = {
+    'https://cdn.octii.chat/assets/default.webp'
+  },
+  voice_servers = {
+    ['8f50bcb4-996e-4182-8f78-abdc6cdf7756'] = {
+      public_url = 'wss://voice.octii.chat',
+      private_url = 'http://192.168.0.181:8081',
+    }
+  },
+  voice_token = '/run/secrets/voice_token',
+  ssl_certificate = '/etc/ssl/cert.pem',
+  port = 8086,
+  pushpin = 'http://pushpin:5561',
+  push = 'http://push:8080',
+  resolver = '127.0.0.11',
+  subscriptions_webhook = '/run/secrets/subscriptions_webhook'
 })
